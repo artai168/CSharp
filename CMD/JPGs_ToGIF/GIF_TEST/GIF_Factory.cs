a497ed519222f812bcfb64c6ea2d53b3f25069e1
artai168 <47489938+artai168@users.noreply.github.com>
2023-08-27 12:00:10 +0800

    Sample code for converting 2 JPGs to an animated GIF

---
+++ CMD/JPGs_ToGIF/GIF_TEST/GIF_Factory.cs
@@ -0,0 +1,35 @@
+﻿using ImageMagick;
+
+namespace GIF_TEST
+{
+    public class GIF_Factory
+    {
+        //Convert 2 JPG files to an animation GIF
+        public void JPG_To_Image(string in_jpg_1, string in_jpg_2, string out_Img_Result)
+		{
+			using (MagickImageCollection collection = new MagickImageCollection())
+			{
+				// Add first image and set the animation delay to 100ms
+				collection.Add(in_jpg_1);
+				collection[0].AnimationDelay = 100;
+
+				// Add second image, set the animation delay to 100ms and flip the image
+				collection.Add(in_jpg_2);
+				collection[1].AnimationDelay = 100;
+				//collection[1].Flip();
+
+				// Optionally reduce colors
+				QuantizeSettings settings = new QuantizeSettings();
+				settings.Colors = 256;
+				collection.Quantize(settings);
+
+				// Optionally optimize the images (images should have the same size).
+				collection.Optimize();
+
+				// Save gif
+				collection.Write(out_Img_Result);
+			}
+		}
+    }
+
+}
---
+++ CMD/JPGs_ToGIF/GIF_TEST/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GIF_TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _jpg1 = @"D:\Family\20161226_133654.jpg";
+            string _jpg2 = @"D:\Family\20161226_133655.jpg";
+            string _result_GIF = @"D:\park_20161226_1336_.gif";
+
+            GIF_Factory gif_Factory = new GIF_Factory();
+            gif_Factory.JPG_To_Image(_jpg1, _jpg2, _result_GIF);
+            Console.ReadLine();
+        }
+    }
+}
